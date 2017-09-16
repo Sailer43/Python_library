fc040aab6614ecc18b8b3278ccd2b224a4426605
Sitao Chen <sitaochen@sitaos-air.attlocal.net>
2017-09-16 00:28:35 -0400

    debugged linked list

--- Collection/__linked_list__.py
+++ Collection/__linked_list__.py
@@ -9,20 +9,20 @@ class __linked_list__:
     class __node__:
         """TO_DO"""
         
-        def __init__(self, data = None):
+        def __init__(self, list_refer, data = None):
             self.data = data
-            if (__linked_list__.has_next):
+            if (list_refer.has_next):
                 self.post_node = None
-            if (__linked_list__.has_previous):
+            if (list_refer.has_previous):
                 self.pre_node = None
 
-        def set_data(data):
+        def set_data(self, data):
             self.data = data
                 
-        def set_post(post_node):
+        def set_post(self, post_node):
             self.post_node = post_node
             
-        def set_pre(pre_node):
+        def set_pre(self, pre_node):
             self.pre_node = pre_node
             
     def __init__(self, data : list = None,
@@ -31,14 +31,14 @@ class __linked_list__:
         self.has_previous = has_previous
         if (self.has_next and self.has_previous):
             #double linked
-            self.first_smart_node = __node__()
-            self.last_smart_node = __node__()
+            self.first_smart_node = self.__node__(self)
+            self.last_smart_node = self.__node__(self)
             pre_node = self.first_smart_node
             last_node = self.last_smart_node
             pre_node.set_post(last_node)
             last_node.set_pre(pre_node)
             for element in data:
-                n = __node__(element)
+                n = self.__node__(self, element)
                 pre_node.set_post(n)
                 last_node.set_pre(n)
                 n.set_post(last_node)
@@ -46,18 +46,18 @@ class __linked_list__:
                 pre_node = n
         elif (self.has_next):
             #backward linked
-            self.first_smart_node = self.__node__()
+            self.first_smart_node = self.__node__(self)
             pre_node = self.first_smart_node
             for element in data:
-                n = __node__(element)
+                n = self.__node__(self, element)
                 pre_node.set_post(n)
                 pre_node = n
         elif (self.has_previous):
             #forward linked
-            self.last_smart_node = __node__()
-            last_node = self.first_smart_node
+            self.last_smart_node = self.__node__(self)
+            last_node = self.last_smart_node
             for element in data:
-                n = __node__(element)
+                n = self.__node__(self, element)
                 n.set_pre(last_node.pre_node)
                 last_node.set_pre(n)
         else:
@@ -66,21 +66,20 @@ class __linked_list__:
 
     def __iter__(self):
         if (self.has_next):
-            self.it_pointer = self.first_smart_node.post_node
+            self.it_pointer = self.first_smart_node
         else:
-            self.it_pointer = self.last_smart_node.pre_node
-        return self.it_pointer.data
+            self.it_pointer = self.last_smart_node
+        return self
 
     def __next__(self):
         if (self.has_next):
-            if (self.it_pointer.post_node == None):
-                self.it_pointer = None
+            if (self.it_pointer.post_node == None
+                    or self.it_pointer.post_node == self.last_smart_node):
                 raise StopIteration
             else:
                 self.it_pointer = self.it_pointer.post_node
         else:
             if (self.it_pointer.pre_node == None):
-                self.it_pointer = None
                 raise StopIteration
             else:
                 self.it_pointer = self.it_pointer.pre_node
--- Collection/test.py
+++ Collection/test.py
@@ -5,6 +5,6 @@ __main__ = "test"
 import __linked_list__
 
 a = [1, 2, 3, 4]
-b = __linked_list__.__linked_list__(a)
+b = __linked_list__.__linked_list__(a, has_next = False, has_previous = True)
 for element in b:
-    print(element + '\n')
+    print(element)
